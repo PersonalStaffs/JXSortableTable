b9d5514dcb6e9a82dc7df222c9d5ca20b0e66dee
sotir <sotir@sovadi>
2016-01-25 13:22:40 +0200

    JXSortableTable small change to comments

--- src/org/riflemansd/jxsortabletable/JXSortableTable.java
+++ src/org/riflemansd/jxsortabletable/JXSortableTable.java
@@ -10,7 +10,8 @@ import org.jdesktop.swingx.JXTable;
 
 /**
  * JXSortableTable 
- * A JXTable that can be sorted by every column, with diffrent ways, depend on column values
+ * A JXTable that can be sorted by every column by clicking at header
+ * Sort depend on column values: 
  * Numeric values will be sorted as numeric
  * String values will be sorted alpharithmetic
  * Date values will be sorted as Dates
